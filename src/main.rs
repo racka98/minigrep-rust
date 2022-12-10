713f7a5cc1613f5330261eea53da0fa33e0b806c
racka98 <bmkilaha@gmail.com>
2022-12-10 20:21:55 +0300

    refactor code for proper error handling and testing

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,40 @@
-use std::{env, fs};
+use std::{env, error::Error, fs, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    println!("Searching for {}", query);
-    println!("In file {}\n", file_path);
+    println!("Searching for {}", config.query);
+    println!("In file {}\n", config.file_path);
 
-    let contents = fs::read_to_string(file_path)
-        .expect("Should be able to read the file!");
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
+}
 
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
     println!("With text contents:\n{}", contents);
+    Ok(())
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("No enough arguments");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        Ok(Config { query, file_path })
+    }
 }
