112003c4edfaada3ea72d1c6ad142865959539dd
racka98 <bmkilaha@gmail.com>
2022-12-10 19:28:15 +0300

    read poem file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::{env, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -7,5 +7,10 @@ fn main() {
     let file_path = &args[2];
 
     println!("Searching for {}", query);
-    println!("In file {}", file_path);
+    println!("In file {}\n", file_path);
+
+    let contents = fs::read_to_string(file_path)
+        .expect("Should be able to read the file!");
+
+    println!("With text contents:\n{}", contents);
 }
