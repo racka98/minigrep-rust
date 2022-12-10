167a23d97e5c6f21e10338378c8ec3b86127ff57
racka98 <bmkilaha@gmail.com>
2022-12-10 20:30:00 +0300

    move logic to lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::{env, error::Error, fs, process};
+use std::{env, process};
+
+use minigrep::run;
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -14,27 +17,5 @@ fn main() {
     if let Err(e) = run(config) {
         println!("Application error: {}", e);
         process::exit(1);
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-    println!("With text contents:\n{}", contents);
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("No enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-        Ok(Config { query, file_path })
     }
 }
